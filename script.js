37c3c7207857f4f0ad5e2552f6e24097143fc976
NBS PROJECT <itsuko.suzuki.s@nbs.edu.mx>
2025-05-03 16:57:08 -0700

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-function mostrarMensaje() {
-    const mensaje = document.getElementById("mensaje");
-    mensaje.textContent = "La inteligencia artificial puede aprender de grandes volúmenes de datos y mejorar con el tiempo, lo que la hace extremadamente poderosa en el análisis predictivo.";
-}
