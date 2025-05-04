5893ff3830275ddafa1fd6397c1bed8b6ba02ce1
NBS PROJECT <itsuko.suzuki.s@nbs.edu.mx>
2025-05-03 18:14:53 -0700

    Update script.js

--- script.js
+++ script.js
@@ -3,18 +3,18 @@ document.addEventListener('DOMContentLoaded', function () {
     const navLinks = document.querySelectorAll('nav a');
 
     window.addEventListener('scroll', () => {
-        let currentSectionId = '';
+       let current= ''; let currentSectionId = '';
 
         sections.forEach(section => {
-            const sectionTop = section.offsetTop - 100; // margen más preciso
-            const sectionHeight = section.offsetHeight;
-
+  const sectionTop= section.offsetTop - 80;  const sectionTop = section.offsetTop - 100; // margen más preciso
+            if (scrolly>= sectionTop) { const sectionHeight = section.offsetHeight;
+current = sectionTop.getAttribute('id');
             if (window.scrollY >= sectionTop && window.scrollY < sectionTop + sectionHeight) {
                 currentSectionId = section.getAttribute('id');
             }
         });
 
-        navLinks.forEach(link => {
+         if (link.getAttribute('herf')=== '#${current}'){ navLinks.forEach(link => {
             link.classList.remove('active');
 
             const href = link.getAttribute('href').substring(1); // Elimina el #
